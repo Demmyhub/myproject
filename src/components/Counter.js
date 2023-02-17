86ce824f4cc49a8d89763a9e8a1a8acc25994302
Demmyhub <parbdul@gmail.com>
2023-02-17 05:05:18 +0100

    this is counter application with react

---
+++ src/components/Counter.js
@@ -0,0 +1,25 @@
+import { Component } from "react";
+
+class Counter extends Component{
+    constructor(){
+        super()
+        this.state ={
+            counter:0,
+        }
+    }
+    increment(){
+        this.setState({
+            counter: this.state.counter + 1
+        })
+    }
+    render(){
+        return (
+            <div>
+                <h1>Counter value is: {this.state.counter}</h1>
+                <button onClick={()=> this.increment()}>Click</button>
+            </div>
+        )
+    }
+}
+
+export default Counter
